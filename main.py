0df3d5e867588afdd7ed58fd10bcc2e880f2b6b4
Alex Yen <alexanderyen88@gmail.com>
2024-07-01 19:02:19 -0700

    API Works

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-def main():
-    print("Hello World")
+import datetime as dt
+import requests 
 
-main()
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+API_KEY = "d274d311bcd15678b29f58537a8dfa4d"
+CITY = "San Diego"
+
+url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+
+response = requests.get(url).json()
+
+print(response)
+

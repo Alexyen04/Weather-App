9b70ba3bd15d357a5191683934062de03e58bae3
sshakya03 <github-registered-email>
2024-07-02 15:58:32 -0700

    Created window and implemented tkinter

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import tkinter as tk
+from tkinter import ttk
 import datetime as dt
 import requests 
 
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
-CITY = "San Diego"
+# window
+window = tk.Tk()
+window.title('Weather App')
+window.geometry('500x750')
 
-url = BASE_URL + "appid=" + "251e61a1560524440fb982133cbce2da" + "&q=" + CITY
-
-reponse = requests.get(url).json()
-
-print(reponse)
+# run
+window.mainloop()

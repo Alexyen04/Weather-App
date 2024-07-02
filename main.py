b4cd2df273dedb3b07e1dc936b5113910710ff21
sshakya03 <github-registered-email>
2024-07-01 19:03:52 -0700

    Implemented weather API

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def main():
-    print("Hello World")
+import datetime as dt
+import requests 
 
-main()
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+CITY = "San Diego"
+
+url = BASE_URL + "appid=" + "251e61a1560524440fb982133cbce2da" + "&q=" + CITY
+
+reponse = requests.get(url).json()
+
+print(reponse)

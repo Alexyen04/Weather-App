372a045f7257ad4b2e0b9c2ffd49de97602c606d
Alex Yen <alexanderyen88@gmail.com>
2024-07-08 16:53:58 -0700

    Added user input for different cities

--- main.py
+++ main.py
@@ -3,65 +3,89 @@ from tkinter import ttk
 import datetime as dt
 import requests 
 
+import window 
+
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 API_KEY = "d274d311bcd15678b29f58537a8dfa4d"
 CITY = "San Diego"
+
+url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+response = requests.get(url).json()
+#print(response)
 
 def kelvinToCelciusAndFahrenheit(kelvin) :
     celsius = kelvin -273.15
     fahrenheit = (celsius * (9/5)) + 32
     return celsius, fahrenheit
 
+def getData(): 
+    #Actual Temperature
+    tempKelvin = response['main']['temp']
+    tempCelsius, tempFahrenheit = kelvinToCelciusAndFahrenheit(tempKelvin)
 
-url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
-response = requests.get(url).json()
-print(response)
+    #Feels Like Temperature
+    feelsLikeTempKelvin = response['main']['feels_like']
+    feelsLikeTempCelsius, feelsLikeTempFahrenheit = kelvinToCelciusAndFahrenheit(feelsLikeTempKelvin)
 
-#Actual Temperature
-tempKelvin = response['main']['temp']
-tempCelsius, tempFahrenheit = kelvinToCelciusAndFahrenheit(tempKelvin)
+    #Temp Min
+    tempMinKelvin = response['main']['temp_min']
+    tempMinCelsius, tempMinFahrenheit = kelvinToCelciusAndFahrenheit(tempMinKelvin)
 
-#Feels Like Temperature
-feelsLikeTempKelvin = response['main']['feels_like']
-feelsLikeTempCelsius, feelsLikeTempFahrenheit = kelvinToCelciusAndFahrenheit(feelsLikeTempKelvin)
+    #Temp Max
+    tempMaxKelvin = response['main']['temp_max']
+    tempMaxCelsius, tempMaxFahrenheit = kelvinToCelciusAndFahrenheit(tempMaxKelvin)
 
-#Temp Min
-tempMinKelvin = response['main']['temp_min']
-tempMinCelsius, tempMinFahrenheit = kelvinToCelciusAndFahrenheit(tempMinKelvin)
+    #Pressure 
+    pressure = response['main']['pressure']
 
-#Temp Max
-tempMaxKelvin = response['main']['temp_max']
-tempMaxCelsius, tempMaxFahrenheit = kelvinToCelciusAndFahrenheit(tempMaxKelvin)
+    #Humidity
+    humidity = response['main']['humidity']
 
-#Pressure 
-pressure = response['main']['pressure']
+    #Sea Level
+    seaLevel = response['main']['sea_level']
 
-#Humidity
-humidity = response['main']['humidity']
+    #Description
+    description = response['weather'][0]['description']
 
-#Sea Level
-seaLevel = response['main']['sea_level']
+    #Windspeed
+    windspeed = response['wind']['speed']
 
-#Description
-description = response['weather'][0]['description']
+    #Time 
+    timezoneDifference = response['timezone']
+    currentTime = (dt.datetime.now(dt.timezone.utc) + dt.timedelta(seconds=timezoneDifference))
+    naive_currentTime = currentTime.replace(tzinfo=None)
+    print(naive_currentTime)
 
-#Windspeed
-windspeed = response['wind']['speed']
+    #print(f"Temperature in {CITY}: {tempCelsius:.2f}°C or {tempFahrenheit:2f}°F")
+    #print(f"Temperature in {CITY} feels like: {feelsLikeTempCelsius:.2f}°C or {feelsLikeTempFahrenheit:.2f}°F")
 
-#Time 
-timezoneDifference = response['timezone']
-currentTime = (dt.datetime.now(dt.timezone.utc) + dt.timedelta(seconds=timezoneDifference))
-naive_currentTime = currentTime.replace(tzinfo=None)
 
-print(naive_currentTime)
+class GUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title('Weather App')
+        self.root.geometry('500x750')
 
-#print(f"Temperature in {CITY}: {tempCelsius:.2f}°C or {tempFahrenheit:2f}°F")
-#print(f"Temperature in {CITY} feels like: {feelsLikeTempCelsius:.2f}°C or {feelsLikeTempFahrenheit:.2f}°F")
+        self.input = tk.Text(self.root, height = 1, font = ('Arial', 15))
+        self.input.pack(padx=10, pady=10)
+        
+        self.enterBtn = tk.Button(self.root, text= "Enter", font = ('Arial', 15), command=self.search)
+        self.enterBtn.pack(padx=10, pady=10)
 
-# window
-window = tk.Tk()
-window.title('Weather App')
-window.geometry('500x750')
+        self.root.mainloop()
+    
+    def search(self) :
+        CITY = self.input.get('1.0', tk.END)
+        print(CITY)
 
-# run
-window.mainloop()
+        url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+        response = requests.get(url).json()
+        getData()
+        #print(response)
+        
+
+
+GUI()
+
+
+
